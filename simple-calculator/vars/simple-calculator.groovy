31d06a7bd666d4f88516d8130aa21770f767eb78
TVenkataManikanta <venkatamanikanta.t@gmail.com>
2023-06-23 15:02:18 +0530

    Update simple-calculator.groovy

--- simple-calculator/vars/simple-calculator.groovy
+++ simple-calculator/vars/simple-calculator.groovy
@@ -3,27 +3,27 @@ def call(num1, num2) {
 }
 
 def add(num1, num2) {
-    return "The addition of the numbers ${num1} and ${num2} is ${num1 + num2}"
+    return "The addition of the numbers ${num1} and ${num2} is ${num1.toInteger() + num2.toInteger()}"
 }
 
 def subtract(num1, num2) {
-    return "The subtraction of the numbers ${num1} and ${num2} is ${num1 - num2}"
+    return "The subtraction of the numbers ${num1} and ${num2} is ${num1.toInteger() - num2.toInteger()}"
 }
 
 def multiply(num1, num2) {
-    return "The multiplication of the numbers ${num1} and ${num2} is ${num1 * num2}"
+    return "The multiplication of the numbers ${num1} and ${num2} is ${num1.toInteger() * num2.toInteger()}"
 }
 
 
 def oddoreven(num1, num2) {
-    if (num1 % 2 == 0) {
+    if (num1.toInteger() % 2 == 0) {
         echo "Number 1 is Even"
     }
     else {
         echo "Number 1 is Odd"
     }
 
-    if (num2 % 2 == 0) {
+    if (num2.toInteger() % 2 == 0) {
         echo "Number 2 is Even"
     }
     else {
